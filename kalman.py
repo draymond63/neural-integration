519ecd820c68e2b7c9705211112dae63d901d273
Daniel Raymond <dan@raymond.ch>
2024-03-23 19:07:08 -0400

    Better plotting

--- kalman.py
+++ kalman.py
@@ -1,13 +1,15 @@
 import numpy as np
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+from scipy.stats import multivariate_normal
+from typing import Tuple
 
 
 np.random.seed(0)
 
 
 class Agent:
-    def __init__(self, init_state: np.ndarray, dt: float, init_uncertainty=1e-1, process_noise=1e-2, measurement_noise=1e-1):
+    def __init__(self, init_state: np.ndarray, dt: float, init_uncertainty=1e-1, process_noise=1e-2, measurement_noise=1e-2):
         if len(init_state) != 4:
             raise ValueError("Initial state must be a 4D vector [x, y, vx, vy]")
         self.x = np.array(init_state)
@@ -32,8 +34,8 @@ class Agent:
         self.x += K @ y
         self.P = self.P - K @ self.H @ self.P
 
-    def get_std(self):
-        return np.diag(self.P)[:2]
+    def get_pos_cov(self):
+        return self.P[:2, :2]
 
 
 def simulate(path: np.ndarray, dt=0.01):
@@ -43,15 +45,15 @@ def simulate(path: np.ndarray, dt=0.01):
     agent = Agent(init_state, dt=dt)
     positions = np.zeros((len(path), 2))
     positions[0] = path[0]
-    confidence = np.zeros((len(path), 2))
-    confidence[0] = agent.get_std()
+    covariances = np.zeros((len(path), 2, 2))
+    covariances[0] = agent.get_pos_cov()
 
     for i in range(1, len(path)):
         agent.predict()
         positions[i] = agent.x[:2]
-        confidence[i] = agent.get_std()
+        covariances[i] = agent.get_pos_cov()
         agent.update(vels[i-1])
-    return timestamps, positions, confidence
+    return timestamps, positions, covariances
 
 
 
@@ -67,6 +69,31 @@ def generate_path(n_steps, domain_dim, smoothing_window=200):
     return path
 
 
+def plot_bounded_path(ts: np.ndarray, *paths: Tuple[np.ndarray, np.ndarray]):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.1)
+    dims = ['x', 'y']
+    colors = ['blue', 'red', 'green', 'purple', 'orange']
+    bound_params = dict(line=dict(dash='dash'), showlegend=False, mode='lines', col=1)
+
+    for idx, (path, covariances) in enumerate(paths):
+        design_params = dict(mode='lines', showlegend=len(paths) > 1, col=1)
+        design_params['marker'] = dict(color=colors[idx])
+        bound_params['marker'] = dict(color=colors[idx])
+        for dim, dim_label in enumerate(dims):
+            design_params['row'] = dim + 1
+            bound_params['row'] = dim + 1
+            stds = np.sqrt(np.diagonal(covariances, axis1=1, axis2=2))    
+            fig.add_scatter(x=ts, y=path[:,dim], name=f"Path {idx}", **design_params)
+            fig.add_scatter(x=ts, y=path[:,dim] + stds[:,dim], **bound_params)
+            fig.add_scatter(x=ts, y=path[:,dim] - stds[:,dim], **bound_params)
+            design_params['showlegend'] = False
+
+    for i, dim_label in enumerate(dims, 1):
+        fig.update_yaxes(title_text=f"{dim_label.capitalize()}", row=i, col=1)
+    fig.update_xaxes(title_text="Time (s)", row=len(dims), col=1)
+    fig.show()
+
+
 def get_map_space(positions: np.ndarray, ppm=1):
     """Returns 2x2 array of maps"""
     max_dims = np.max(positions, axis=0) + 1
@@ -77,35 +104,17 @@ def get_map_space(positions: np.ndarray, ppm=1):
     ys = np.linspace(min_dims[1], max_dims[1], points[1])
     return xs, ys
 
-def gaussian_2d_point(x, y, mu_x, mu_y, std_x, std_y):
-    """Returns the value of the 2D Gaussian distribution at (x, y)"""
-    term1 = 1 / (2 * np.pi * std_x * std_y)
-    term2 = np.exp(-((x - mu_x)**2 / (2 * std_x**2) + (y - mu_y)**2 / (2 * std_y**2)))
-    return term1 * term2
+def gaussian_2d(xs, ys, mean, cov):
+    mean = np.array([0, 0])
+    cov = np.array([[1, 0], [0, 1]])
+    rv = multivariate_normal(mean, cov)
+    return np.array([[rv.pdf([x, y]) for x in xs] for y in ys])
 
-def gaussian_2d(xs, ys, mu, std):
-    x, y = np.meshgrid(xs, ys)
-    return gaussian_2d_point(x, y, *mu, *std)
-
-
-def compare_paths(ts: np.ndarray, path1: np.ndarray, path2: np.ndarray):
-    fig = make_subplots(rows=2, cols=1)
-    fig.add_trace(go.Scatter(x=ts, y=path1[:,0], mode='lines', name='Path 1'), row=1, col=1)
-    fig.add_trace(go.Scatter(x=ts, y=path2[:,0], mode='lines', name='Path 2'), row=1, col=1)
-    fig.add_trace(go.Scatter(x=ts, y=path1[:,1], mode='lines', name='Path 1'), row=2, col=1)
-    fig.add_trace(go.Scatter(x=ts, y=path2[:,1], mode='lines', name='Path 2'), row=2, col=1)
-    fig.show()
-    # fig = go.Figure()
-    # fig.add_trace(go.Scatter(x=path1[:0], y=path1[:,1], mode='lines', name='Path 1'))
-    # fig.add_trace(go.Scatter(x=path2[:0], y=path2[:,1], mode='lines', name='Path 2'))
-    # fig.show()
-
-
-def animate(positions: np.ndarray, confidences: np.ndarray, ppm: int=10):
+def animate(positions: np.ndarray, covariances: np.ndarray, ppm: int=10):
     xs, ys = get_map_space(positions, ppm)
-    dists = np.array([gaussian_2d(xs, ys, pos, std) for pos, std in zip(positions, confidences)])
+    dists = np.array([gaussian_2d(xs, ys, pos, cov) for pos, cov in zip(positions, covariances)])
+    print(dists.shape)
     max_amp = np.max(dists, axis=None)
-    # print(dists.shape)
     heatmaps = [go.Heatmap(x=xs, y=ys, z=dist, zmax=max_amp, colorscale='Viridis') for dist in dists]
     frames = [go.Frame(data=[heatmap]) for heatmap in heatmaps]
     fig = go.Figure(
@@ -126,7 +135,6 @@ def animate(positions: np.ndarray, confidences: np.ndarray, ppm: int=10):
 
 
 if __name__ == "__main__":
-    path = generate_path(100, 2)
-    timestamps, positions, confidences = simulate(path)
-    compare_paths(timestamps, path, positions)
-    animate(positions, confidences)
+    path = generate_path(1000, 2)
+    timestamps, positions, covariances = simulate(path)
+    plot_bounded_path(timestamps, [positions, covariances])
